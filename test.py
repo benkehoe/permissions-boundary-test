a89141034799d7f4ab76ed065ce586488478f027
Ben Kehoe <ben@kehoe.io>
2021-09-12 17:34:57 -0600

    Updated

--- test.py
+++ test.py
@@ -51,6 +51,20 @@ def run_test(bucket, keys, body):
             results.append((key, False))
         except aws_error_utils.ClientError as e:
             results.append((key, e))
+
+    for base_key in keys:
+        key = "C-" + base_key
+        try:
+            bucket.put_object(
+                Key=key,
+                Body=body,
+            )
+            results.append((key, True))
+        except aws_error_utils.errors.AccessDenied:
+            results.append((key, False))
+        except aws_error_utils.ClientError as e:
+            results.append((key, e))
+
     return results
 
 def print_results(results):
@@ -61,6 +75,11 @@ def print_results(results):
         else:
             result_str = str(result)
         print(f"{key.ljust(max_length)} {result_str}")
+
+def delete_objects(session, bucket_name):
+    bucket = session.resource("s3").Bucket(bucket_name)
+    for obj_summary in bucket.objects.all():
+        obj_summary.delete()
 
 timestamp = datetime.datetime.now(tz=datetime.timezone.utc).isoformat()
 timestamp_bytes = timestamp.encode()
@@ -80,21 +99,6 @@ cloudformation = session.client("cloudformation")
 
 try:
     response = cloudformation.describe_stacks(StackName=args.stack_name)
-    _, parameters, outputs = get_stack_info(response)
-    if parameters["AssumedRoleSessionArn"] != EMPTY_PARAM:
-        print("Resetting AssumedRoleSessionArn")
-        cloudformation.update_stack(
-            StackName=args.stack_name,
-            UsePreviousTemplate=True,
-            Parameters=[{
-                "ParameterKey": "AssumedRoleSessionArn",
-                "ParameterValue": EMPTY_PARAM,
-            }],
-            Capabilities=["CAPABILITY_IAM"],
-        )
-        print("Update in progress")
-        cloudformation.get_waiter("stack_update_complete").wait(StackName=args.stack_name)
-        print("Update complete")
 except aws_error_utils.errors.ValidationError:
     print("Stack doesn't exist, creating")
     cloudformation.create_stack(
@@ -106,6 +110,23 @@ except aws_error_utils.errors.ValidationError:
     print("Create in progress")
     cloudformation.get_waiter("stack_create_complete").wait(StackName=args.stack_name)
     print("Create complete")
+else:
+    print("Resetting stack")
+    try:
+        response = cloudformation.update_stack(
+            StackName=args.stack_name,
+            TemplateBody=template,
+            Parameters=[{
+                "ParameterKey": "AssumedRoleSessionArn",
+                "ParameterValue": EMPTY_PARAM,
+            }],
+            Capabilities=["CAPABILITY_IAM"],
+        )
+        print("Update in progress")
+        cloudformation.get_waiter("stack_update_complete").wait(StackName=args.stack_name)
+        print("Update complete")
+    except aws_error_utils.errors.ValidationError:
+        print("Stack did not need update")
 
 response = cloudformation.describe_stacks(StackName=args.stack_name)
 _, _, outputs = get_stack_info(response)
@@ -141,3 +162,6 @@ print("\n\nTesting assumed role session as resource policy principal")
 results = run_test(bucket, KEYS, timestamp_bytes)
 
 print_results(results)
+
+print("\n\nDeleting objects from bucket")
+delete_objects(session, outputs["BucketName"])
